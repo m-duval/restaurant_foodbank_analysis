25c5633a4edd8c9fe1f5bd46bcea72e5fcfacaf2
MICHAEL DUVAL <duv74@hotmail.com>
2019-10-09 15:14:40 -0400

    clean up

--- michael_mapping_hunger/visual_studio/longlat.js
+++
@@ -1,7 +0,0 @@
-$latlongUrl = 'http://maps.googleapis.com/maps/api/geocode/json?components=postal_code:'.$zipcode;
-
-    $data = "data/hunger.csv[Zipcode]"($latlongUrl); // you will get string data
-
-    $data = (json_decode($data)); // convert it into object with json_decode
-
-    $location = ($data == results[0] == geometry == location); // get location object
