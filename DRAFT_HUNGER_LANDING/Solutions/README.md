94fe99d0b47159fbc8d70c0aa00714b1eefd9f74
JAK-UCF <jenklimek@msn.com>
2019-10-12 13:14:42 -0400

    cleanup and submission

--- DRAFT_HUNGER_LANDING/Solutions/README.md
+++
@@ -1,3 +0,0 @@
-## Grading
-
-* Follow the [Web_Design_Grading_Rubric.pdf](../Instructions/Web_Design_Grading_Rubric.pdf) for grading instructions.
--- dropdown.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-     $("#dropdown_change").change(function(){
-      alert("Selected value is : " + document.getElementById("dropdown_change").value);
-     });
-   });
